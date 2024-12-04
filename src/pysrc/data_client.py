6b9210bc9a3cb4ec5efac97c290769ec1dd4acef
Nathan Wang <nwangjs@gmail.com>
2024-12-04 16:28:06 -0600

    Add linted/formatted initial data_client.py implementation

--- src/pysrc/data_client.py
+++ src/pysrc/data_client.py
@@ -3,21 +3,23 @@ import requests
 import json
 
 BASE_URL = "https://api.gemini.com/v1/trades/"
+
+
 class DataClient:
     def __init__(self) -> None:
         self.data = {}
 
     def _query_api(self, symbol: str) -> None:
-        try: 
+        try:
             response = requests.get(BASE_URL + symbol)
             trade_history = response.json()
             self._parse_message(symbol, trade_history)
         except Exception as e:
             print(f"API Query Failed: {e}", file=sys.stderr)
 
-    def _parse_message(self, symbol:str, trade_history: dict) -> None:
+    def _parse_message(self, symbol: str, trade_history: dict) -> None:
         self.data[symbol] = trade_history
 
     def get_data(self, symbol: str) -> None:
-       self._query_api(symbol)
-       print(self.data[symbol])
+        self._query_api(symbol)
+        print(self.data.get(symbol))
