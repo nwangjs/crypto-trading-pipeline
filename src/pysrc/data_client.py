d1a5a6add8dbca6900b303b6bf2035871a27172f
Nathan Wang <nwangjs@gmail.com>
2024-12-04 16:24:57 -0600

    Add initial implementation of data_client.py

--- src/pysrc/data_client.py
+++ src/pysrc/data_client.py
@@ -1,12 +1,23 @@
+import sys
+import requests
+import json
+
+BASE_URL = "https://api.gemini.com/v1/trades/"
 class DataClient:
     def __init__(self) -> None:
-        raise NotImplementedError
+        self.data = {}
 
-    def _query_api(self) -> None:
-        raise NotImplementedError
+    def _query_api(self, symbol: str) -> None:
+        try: 
+            response = requests.get(BASE_URL + symbol)
+            trade_history = response.json()
+            self._parse_message(symbol, trade_history)
+        except Exception as e:
+            print(f"API Query Failed: {e}", file=sys.stderr)
 
-    def _parse_message(self, message: str) -> None:
-        raise NotImplementedError
+    def _parse_message(self, symbol:str, trade_history: dict) -> None:
+        self.data[symbol] = trade_history
 
-    def get_data(self) -> None:
-        raise NotImplementedError
+    def get_data(self, symbol: str) -> None:
+       self._query_api(symbol)
+       print(self.data[symbol])
