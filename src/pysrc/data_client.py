8a5c67dbdffae585036b49240c51820076d80477
Nathan Wang <nwangjs@gmail.com>
2024-12-04 00:07:26 -0600

    Add formatted src/pysrc files

--- src/pysrc/data_client.py
+++ src/pysrc/data_client.py
@@ -1,12 +1,12 @@
 class DataClient:
-    def __init__(self):
+    def __init__(self) -> None:
         raise NotImplementedError
 
     def _query_api(self) -> None:
         raise NotImplementedError
 
-    def _parse_message(self, message):
+    def _parse_message(self, message: str) -> None:
         raise NotImplementedError
 
-    def get_data(self):
+    def get_data(self) -> None:
         raise NotImplementedError
