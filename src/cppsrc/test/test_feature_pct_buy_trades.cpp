aab849f3405ddafac01a98758d88a5da2368bc87
Nathan Wang <nwangjs@gmail.com>
2025-01-02 10:48:41 -0600

    Add PercentSellFeature

--- src/cppsrc/test/test_feature_pct_buy_trades.cpp
+++ src/cppsrc/test/test_feature_pct_buy_trades.cpp
@@ -6,7 +6,7 @@ using namespace intproj;
 TEST(FeatureTests, PctBuyTest)
 {
     PercentBuyFeature ptf;
-    EXPECT_EQ(ptf.compute_feature({ { 1, 1, false } }), 0);
-    EXPECT_EQ(ptf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 0.5);
-    EXPECT_EQ(ptf.compute_feature({ { 1, 1, true } }), 1);
+    EXPECT_NEAR(ptf.compute_feature({ { 1, 1, false } }), 0, 1e-2);
+    EXPECT_NEAR(ptf.compute_feature({ { 1, 1, false }, { 1, 1, true } }), 0.5, 1e-2);
+    EXPECT_NEAR(ptf.compute_feature({ { 1, 1, true } }), 1, 1e-2);
 }
